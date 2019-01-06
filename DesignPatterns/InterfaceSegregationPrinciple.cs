c81417177a1b71e2c903cf91416d309df7062562
Furkan Emre <furkan54emre@gmail.com>
2019-01-06 20:27:01 +0300

    Interface Segregation Principle

---
+++ DesignPatterns/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    public class Document
+    {
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Fax(Document d);
+        void Scan(Document d);
+    }
+
+    // If you need a multifucntion machine
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            // We can implement this but what about other methods. What we will do for them?
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Therefore instead divide interface to smaller interfaces
+    // if neccessary build new interfaces using existing interfaces
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Printer : IPrinter
+    {
+        public void Print(Document d)
+        {
+
+        }
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+
+        }
+
+        public void Scan(Document d)
+        {
+
+        }
+    }
+
+    public interface IMultiFunctionDevice : IPrinter, IScanner
+    {
+
+    }
+
+    public class MultiFunctionMachine : IMultiFunctionDevice
+    {
+        // compose this out of several modules
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }
+    }
+}
