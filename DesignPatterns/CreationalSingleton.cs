127057c6952b83217834f626c3de9776ae2ac7a0
Furkan Emre <furkan54emre@gmail.com>
2019-01-28 13:50:40 +0300

    Creational Singleton Monostate

--- DesignPatterns/CreationalSingleton.cs
+++ DesignPatterns/CreationalSingleton.cs
@@ -129,17 +129,17 @@ namespace CreationalSingleton
         }
     }
 
-    public class Demo
-    {
-        static void Main()
-        {
-            var db = SingletonDatabase.Instance;
+    //public class Demo
+    //{
+    //    static void Main()
+    //    {
+    //        var db = SingletonDatabase.Instance;
 
-            // works just fine while you're working with a real database
-            var city = "Tokyo";
-            Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+    //        // works just fine while you're working with a real database
+    //        var city = "Tokyo";
+    //        Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
 
-            // now some tests
-        }
-    }
+    //        // now some tests
+    //    }
+    //}
 }
---
+++ DesignPatterns/CreationalSingletonMonostate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalSingletonMonostate
+{
+    public class ChiefExecutiveOfficer
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new ChiefExecutiveOfficer();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new ChiefExecutiveOfficer();
+            Console.WriteLine(ceo2); // be same as ceo object
+        }
+    }
+}
