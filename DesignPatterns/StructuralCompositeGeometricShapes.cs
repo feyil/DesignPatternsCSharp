021b6cde36de7e08aa4359b5f4b399486c9015f0
Furkan Emre <furkan54emre@gmail.com>
2019-02-01 00:08:30 +0300

    Structural Decorator Code Builder

--- DesignPatterns/StructuralCompositeGeometricShapes.cs
+++ DesignPatterns/StructuralCompositeGeometricShapes.cs
@@ -44,18 +44,18 @@ namespace StructuralCompositeGeometricShapes
 
     public class Demo
     {
-        public static void Main(string[] args)
-        {
-            var drawing = new GraphicObject { Name = "My drawing" };
-            drawing.Children.Add(new Square { Color = "Red" });
-            drawing.Children.Add(new Circle { Color = "Yellow" });
+        //public static void Main(string[] args)
+        //{
+        //    var drawing = new GraphicObject { Name = "My drawing" };
+        //    drawing.Children.Add(new Square { Color = "Red" });
+        //    drawing.Children.Add(new Circle { Color = "Yellow" });
 
-            var group = new GraphicObject();
-            group.Children.Add(new Circle { Color = "Bluıe" });
-            group.Children.Add(new Square { Color = "Blue" });
-            drawing.Children.Add(group);
+        //    var group = new GraphicObject();
+        //    group.Children.Add(new Circle { Color = "Bluıe" });
+        //    group.Children.Add(new Square { Color = "Blue" });
+        //    drawing.Children.Add(group);
 
-            Console.WriteLine(drawing);
-        }
+        //    Console.WriteLine(drawing);
+        //}
     }
 }
