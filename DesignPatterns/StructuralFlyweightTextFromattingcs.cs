d710c839faa2ee0fb89734d077ca031976c788d2
Furkan Emre <furkan54emre@gmail.com>
2019-02-03 14:44:02 +0300

    Structural Protection Proxy

--- DesignPatterns/StructuralFlyweightTextFromattingcs.cs
+++ DesignPatterns/StructuralFlyweightTextFromattingcs.cs
@@ -87,15 +87,15 @@ namespace StructuralFlyweightTextFromattingcs
 
     public class Demo
     {
-        public static void Main(string[] args)
-        {
-            var ft = new FormattedText("This is a brave new world");
-            ft.Capitalize(10, 15);
-            Console.WriteLine(ft);
+        //public static void Main(string[] args)
+        //{
+        //    var ft = new FormattedText("This is a brave new world");
+        //    ft.Capitalize(10, 15);
+        //    Console.WriteLine(ft);
 
-            var bft = new BetterFormattedText("This is a brave new world");
-            bft.GetRange(10, 15).Capitalize = true;
-            Console.WriteLine(bft);
-        } 
+        //    var bft = new BetterFormattedText("This is a brave new world");
+        //    bft.GetRange(10, 15).Capitalize = true;
+        //    Console.WriteLine(bft);
+        //} 
     }
 }
---
+++ DesignPatterns/StructuralProxyProtectionProxy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralProxyProtectionProxy
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car being driven");
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Car car;
+        private Driver driver;
+
+        public CarProxy(Driver driver)
+        {
+            this.car = new Car();
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+            {
+                Console.WriteLine("Driver too young");
+            }
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+
+        public Driver(int age)
+        {
+            Age = age;
+        }
+    }
+
+    public class Demo
+    {
+        public static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver(12)); // 22
+            car.Drive();
+        }
+    }
+}
