ca4e557dcc47739d651e6d78cadba5ab3c9fdf9d
Furkan Emre <furkan54emre@gmail.com>
2019-02-03 22:50:25 +0300

    Behavioral Chaing of Responsiblity Bloker Chain

---
+++ DesignPatterns/BehavioralChainOfResponsibilityBrokerChain.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralChainOfResponsibilityBrokerChain
+{
+    // command query separation is being used here
+
+    public class Query
+    {
+        public string CreatureName { get; set; }
+        
+        public enum Argument
+        {
+            Attack, Defense
+        }
+
+        public Argument WhatToQuery { get; set; }
+        public int Value { get; set; } // bidirectional
+
+        public Query(string creatureName, Argument whatToQuery, int value)
+        {
+            CreatureName = creatureName;
+            WhatToQuery = whatToQuery;
+            Value = value;
+        }
+    }
+
+    public class Game // mediator pattern
+    {
+        public event EventHandler<Query> Queries; // effectively a chain
+
+        public void PerformQuery(object sender, Query q)
+        {
+            Queries?.Invoke(sender, q);
+        }
+    }
+
+    public class Creature
+    {
+        private Game game;
+        public string Name { get; set; }
+        private int attack, defense;
+
+        public Creature(Game game, string name, int attack, int defense)
+        {
+            this.game = game;
+            Name = name;
+            this.attack = attack;
+            this.defense = defense;
+        }
+
+        public int Attack
+        {
+            get
+            {
+                var q = new Query(Name, Query.Argument.Attack, attack);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+
+        public int Defense
+        {
+            get
+            {
+                var q = new Query(Name, Query.Argument.Defense, defense);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+
+        public override string ToString() // no game
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(attack)}: {Attack}, {nameof(defense)}: {Defense}";
+        }
+    }
+
+    public abstract class CreatureModifier : IDisposable
+    {
+        protected Game game;
+        protected Creature creature;
+
+        protected CreatureModifier(Game game, Creature creature)
+        {
+            this.game = game;
+            this.creature = creature;
+            game.Queries += Handle;
+        }
+
+        protected abstract void Handle(object sender, Query q);
+
+        public void Dispose()
+        {
+          //  game.Queries -= Handle;
+        }
+    }
+    
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Game game, Creature creature) : base(game, creature)
+        {
+
+        }
+
+        protected override void Handle(object sender, Query q)
+        {
+           // Console.WriteLine("Handle");
+            if (q.CreatureName == creature.Name &&
+                q.WhatToQuery == Query.Argument.Attack)
+                q.Value *= 2;
+        }
+    }
+
+    public class IncreaseDefenseModifier : CreatureModifier
+    {
+        public IncreaseDefenseModifier(Game game, Creature creature) : base(game, creature)
+        {
+
+        }
+
+        protected override void Handle(object sender, Query q)
+        {
+           // Console.WriteLine("Handle");
+            if (q.CreatureName == creature.Name &&
+                q.WhatToQuery == Query.Argument.Defense)
+                q.Value += 2;
+        }
+    }
+
+    public class Demo
+    {
+        public static void Main(string[] args)
+        {
+            var game = new Game();
+            var goblin = new Creature(game, "Stron Goblin", 3, 3);
+            Console.WriteLine(goblin);
+
+            using(new DoubleAttackModifier(game, goblin))
+            {
+                Console.WriteLine(goblin);
+                using(new IncreaseDefenseModifier(game, goblin))
+                {
+                    Console.WriteLine(goblin);
+                }
+            }
+
+            Console.WriteLine(goblin);
+        }
+    }
+}
--- DesignPatterns/BehavioralChainOfResponsibilityMethodChain.cs
+++ DesignPatterns/BehavioralChainOfResponsibilityMethodChain.cs
@@ -90,24 +90,24 @@ namespace BehavioralChainOfResponsibilityMethodChain
 
     public class Demo
     {
-        public static void Main(string[] args)
-        {
-            var goblin = new Creature("Goblin", 2, 2);
-            Console.WriteLine(goblin);
+        //public static void Main(string[] args)
+        //{
+        //    var goblin = new Creature("Goblin", 2, 2);
+        //    Console.WriteLine(goblin);
 
-            var root = new CreatureModifier(goblin);
+        //    var root = new CreatureModifier(goblin);
 
-           // root.Add(new NoBonusesModifier(goblin));
+        //   // root.Add(new NoBonusesModifier(goblin));
 
-            Console.WriteLine("Let's double goblin's attack..");
-            root.Add(new DoubleAttackModifier(goblin));
+        //    Console.WriteLine("Let's double goblin's attack..");
+        //    root.Add(new DoubleAttackModifier(goblin));
 
-            Console.WriteLine("Let's increase goblin's defense");
-            root.Add(new IncreaseDefenseModifier(goblin));
+        //    Console.WriteLine("Let's increase goblin's defense");
+        //    root.Add(new IncreaseDefenseModifier(goblin));
 
-            // eventually
-            root.Handle();
-            Console.WriteLine(goblin);
-        }
+        //    // eventually
+        //    root.Handle();
+        //    Console.WriteLine(goblin);
+        //}
     }
 }
