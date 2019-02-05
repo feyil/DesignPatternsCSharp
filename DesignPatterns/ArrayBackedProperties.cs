272cc0768666160f482b138064da169c2b97a943
Furkan Emre <furkan54emre@gmail.com>
2019-02-05 15:24:42 +0300

    Array Backed Properties

---
+++ DesignPatterns/ArrayBackedProperties.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBackedProperties
+{
+    public class Creature : IEnumerable<int>
+    {
+        private int[] stats = new int[3];
+        private const int strength = 0;
+
+        public int Strength
+        {
+            get => stats[strength];
+            set => stats[strength] = value;
+        }
+
+        public int Agility
+        {
+            get => stats[1];
+            set => stats[1] = value;
+        }
+        public int Intelligence
+        {
+            get => stats[2];
+            set => stats[2] = value;
+        }
+
+        public double AverageStat => stats.Average();
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return stats.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int this[int index]
+        {
+            get { return stats[index]; }
+            set { stats[index] = value; }
+        }
+
+    }
+
+    public class Demo
+    {
+        //public static void Main(string[] args)
+        //{
+        //    var creature = new Creature();
+        //    creature.Strength = 20;
+        //    creature.Agility = 30;
+        //    creature.Intelligence = 40;
+
+        //    foreach(int i in creature)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //    Console.WriteLine(creature[1]);
+        //}
+    }
+}
--- DesignPatterns/BehavioralIteratorTreeTraversal.cs
+++ DesignPatterns/BehavioralIteratorTreeTraversal.cs
@@ -115,40 +115,40 @@ namespace BehavioralIteratorTreeTraversal
 
     public class Demo
     {
-        public static void Main(string[] args)
-        {
-            //  1
-            // / \
-            //2   3
+        //public static void Main(string[] args)
+        //{
+        //    //  1
+        //    // / \
+        //    //2   3
 
-            // in-order: 213
-            // preorder: 123
-            // postorder: 231
+        //    // in-order: 213
+        //    // preorder: 123
+        //    // postorder: 231
 
-            var root = new Node<int>(1,
-                new Node<int>(2), new Node<int>(3));
+        //    var root = new Node<int>(1,
+        //        new Node<int>(2), new Node<int>(3));
 
-            // C++ style
-            var it = new InOrderIterator<int>(root);
+        //    // C++ style
+        //    var it = new InOrderIterator<int>(root);
 
-            while(it.MoveNext())
-            {
-                Console.Write(it.Current.Value);
-                Console.Write(',');
-            }
-            Console.WriteLine();
+        //    while(it.MoveNext())
+        //    {
+        //        Console.Write(it.Current.Value);
+        //        Console.Write(',');
+        //    }
+        //    Console.WriteLine();
 
-            // C# style
-            var tree = new BinaryTree<int>(root);
+        //    // C# style
+        //    var tree = new BinaryTree<int>(root);
 
-            Console.WriteLine(string.Join(",", tree.NaturalInOrder.Select(x => x.Value)));
+        //    Console.WriteLine(string.Join(",", tree.NaturalInOrder.Select(x => x.Value)));
 
-            // duck typing!
-            foreach(var node in tree)
-            {
-                Console.WriteLine(node.Value);
-            }
-        }
+        //    // duck typing!
+        //    foreach(var node in tree)
+        //    {
+        //        Console.WriteLine(node.Value);
+        //    }
+        //}
     }
 }
 
