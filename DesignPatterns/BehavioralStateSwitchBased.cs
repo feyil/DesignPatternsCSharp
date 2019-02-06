9431977102abd7a429a63a37f15a6697a58d92de
Furkan Emre <furkan54emre@gmail.com>
2019-02-06 21:53:33 +0300

    Behavioral Strategy Dynamic

--- DesignPatterns/BehavioralStateSwitchBased.cs
+++ DesignPatterns/BehavioralStateSwitchBased.cs
@@ -15,43 +15,43 @@ namespace BehavioralStateSwitchBased
 
     public class SwitchBasedDemo
     {
-        public static void Main(string[] args)
-        {
-            string code = "1234";
-            var state = State.Locked;
-            var entry = new StringBuilder();
+        //public static void Main(string[] args)
+        //{
+        //    string code = "1234";
+        //    var state = State.Locked;
+        //    var entry = new StringBuilder();
 
-            while (true)
-            {
-                switch (state)
-                {
-                    case State.Locked:
-                        entry.Append(Console.ReadKey().KeyChar);
+        //    while (true)
+        //    {
+        //        switch (state)
+        //        {
+        //            case State.Locked:
+        //                entry.Append(Console.ReadKey().KeyChar);
 
-                        if (entry.ToString() == code)
-                        {
-                            state = State.Unlocked;
-                            break;
-                        }
+        //                if (entry.ToString() == code)
+        //                {
+        //                    state = State.Unlocked;
+        //                    break;
+        //                }
 
-                        if (!code.StartsWith(entry.ToString()))
-                        {
-                            // the code is blatantly wrong
-                            state = State.Failed;
-                        }
-                        break;
-                    case State.Failed:
-                        Console.CursorLeft = 0;
-                        Console.WriteLine("FAILED");
-                        entry.Clear();
-                        state = State.Locked;
-                        break;
-                    case State.Unlocked:
-                        Console.CursorLeft = 0;
-                        Console.WriteLine("UNLOCKED");
-                        return;
-                }
-            }
+        //                if (!code.StartsWith(entry.ToString()))
+        //                {
+        //                    // the code is blatantly wrong
+        //                    state = State.Failed;
+        //                }
+        //                break;
+        //            case State.Failed:
+        //                Console.CursorLeft = 0;
+        //                Console.WriteLine("FAILED");
+        //                entry.Clear();
+        //                state = State.Locked;
+        //                break;
+        //            case State.Unlocked:
+        //                Console.CursorLeft = 0;
+        //                Console.WriteLine("UNLOCKED");
+        //                return;
+        //        }
+        //   }
         }
     }
-}
+
